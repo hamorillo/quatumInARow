d8797e98ddddda1382b56f2da58bef700e7d3286
Héctor Abraham Morillo Prieto <hectorabrahammp@gmail.com>
2019-05-26 01:13:57 +0200

    Added start method to reset columns history

--- server.py
+++ server.py
@@ -4,20 +4,37 @@ import json
 app = Flask('__name__')
 fileName = 'columnsHistory.csv'
 
+
+@app.route("/start", methods=['POST'])
+def start():
+    clearFile()
+    return ""
+
+
 @app.route("/selectcolumn", methods=['POST'])
-def hello():
+def selectcolumn():
     column = request.args.get('column', type=int)
-    updateFileColumn(column)    
+    updateFileColumn(column)
     return str(column)
+
+def removeFile():
+    open(fileName, 'w').close()
+
+def clearFile():
+    removeFile()
+    with open(fileName, 'r+') as file:
+        data = []
+        file.write(json.dumps(data))
+
 
 def updateFileColumn(column: int):
     data = []
     with open(fileName, 'r') as file:
         data = json.loads(file.read())
         file.close
-    
+
     data.append(column)
-    open(fileName, 'w').close()
+    removeFile()
 
     with open(fileName, 'r+') as file:
         file.write(json.dumps(data))
